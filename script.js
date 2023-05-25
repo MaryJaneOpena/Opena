339ac5781c36b0d2a5d5f907a2a8b55be8a5ad89
MaryJaneOpena <131754005+MaryJaneOpena@users.noreply.github.com>
2023-05-26 03:29:03 +0800

    script.js

--- script.js
+++ script.js
@@ -1,8 +1,1 @@
-// togglr icon navbar
 
-//scroll sections
-window.onscroll = () => {
-    let header = document.queryselector('header');
-
-    header.classlist.toggle('sticky', window.scrolly > 100);
-}
